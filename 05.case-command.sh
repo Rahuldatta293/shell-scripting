7387c6fd6dbfdae5812caa1d99447e262527930f
Rahul datta <bitrarahuldatta@gmail.com>
2020-05-22 11:28:28 +0530

    case-command

---
+++ 05.case-command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TRAINING=$1
+
+case $TRAINING in
+  AWS)
+    echo "Welcome to AWS Training"
+    echo "Batch Timings : 7AM to 8AM IST"
+    echo "Trainer Name : Raju"
+    echo "Demo Date: 17-May"
+    echo "Batch Start Date: 18-May"
+    ;;
+  DEVOPS)
+    echo "Welcome to DEVOPS Training"
+    echo "Batch Timings : 6AM to 7AM IST"
+    echo "Trainer Name : Raju"
+    echo "Demo Date: 31-May"
+    echo "Batch Start Date: 01-June"
+    ;;
